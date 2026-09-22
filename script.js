b6d98c9495465b15f8a6d3228b3ccd59bf0be895
roschreiber <schreiber.robin@icloud.com>
2026-09-22 18:38:54 +0200

    add skew, new colors and match anime design

--- script.js
+++ script.js
@@ -7,6 +7,34 @@ function playBeep() {
   sound.play();
 }
 
+function updateColor() {
+  if (currentCounter <= 9) {
+    counter.style.color = "white";
+  } else if (currentCounter <= 19) {
+    counter.style.color = "lightblue";
+  } else if (currentCounter <= 29) {
+    counter.style.color = "dodgerblue";
+  } else if (currentCounter <= 39) {
+    counter.style.color = "deepskyblue";
+  } else if (currentCounter <= 49) {
+    counter.style.color = "turquoise";
+  } else if (currentCounter <= 59) {
+    counter.style.color = "lime";
+  } else if (currentCounter <= 69) {
+    counter.style.color = "greenyellow";
+  } else if (currentCounter <= 79) {
+    counter.style.color = "yellow";
+  } else if (currentCounter <= 89) {
+    counter.style.color = "gold";
+  } else if (currentCounter <= 94) {
+    counter.style.color = "orange";
+  } else if (currentCounter <= 99) {
+    counter.style.color = "darkorange";
+  } else {
+    counter.style.color = "red";
+  }
+}
+
 function countUp(event) {
   if (currentCounter < 100) {
     if (event.key === "ArrowUp") {
@@ -14,6 +42,7 @@ function countUp(event) {
       counter.innerHTML = currentCounter + "%";
       console.log("+1");
       playBeep();
+      updateColor();
     }
   }
 }
@@ -25,6 +54,7 @@ function countDown(event) {
       counter.innerHTML = currentCounter + "%";
       console.log("-1");
       playBeep();
+      updateColor();
     }
   }
 }
